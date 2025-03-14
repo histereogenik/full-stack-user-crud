62e9d85d42878a26918af1a6cd55661f9a7330e8
Humberto Nacif <gigantedeluz@gmail.com>
2025-03-14 15:14:07 -0300

    test: Preparing testing db and installing dependencies

--- server/db.py
+++ server/db.py
@@ -1,5 +1,12 @@
+import os
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["mydatabase"]
+def get_database(testing=False):
+    uri = os.getenv("MONGO_TEST_URI", "mongodb://localhost:27017/test_database") if testing else os.getenv("MONGO_URI", "mongodb://localhost:27017/mydatabase")
+    client = MongoClient(uri)
+    db = client.get_default_database()
+    return db
+
+
+db = get_database(testing=True)
 users_collection = db["users"]
